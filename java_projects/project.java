e2794cec0e690620e771bf84e7ac0300ce36041c
Evelina <evelina.shaymardanova@list.ru>
2023-06-05 18:36:52 +0500

    added project

---
+++ java_projects/project.java
@@ -0,0 +1,12 @@
+package java_projects;
+
+/**
+ * project
+ */
+public class project {
+    public static void main(String[] args) {
+        int a = 123;
+        a = a-- - --a;
+        System.out.println(a);       
+    }    
+}

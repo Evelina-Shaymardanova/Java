14e19aa80b7f15d73179adb1eb07cbb0d8a27e90
Evelina <evelina.shaymardanova@list.ru>
2023-06-14 15:19:57 +0500

    added seminar3

---
+++ java_seminar3/task1.java
@@ -0,0 +1,64 @@
+package java_seminar3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Пусть дан произвольный список целых чисел.
+
+// 1) Найти максимальное значение
+// 2) Найти минимальное значение
+
+/*import java.util.Collections;
+import java.util.List;
+
+public class task1 {
+    public static void main (String[] args) {
+        List<Integer> list = List.of(1, 12, 123, 1234, 12345);
+        
+        System.out.println(Collections.max(list));
+        System.out.println(Collections.min(list));
+    }
+}
+*/
+    
+  
+
+// 3) Найти среднее значение
+
+/*public class task1 {
+    public static void main (String[] args) {
+        int[] arr = {2, 5, 8, 9, 3, 5, 6};
+        int length = arr.length;
+        int sum = 0;
+        
+        for (int i = 0; i < arr.length; ++i) {
+            sum += arr[i];
+        }
+        System.out.println(sum/length);        
+    }
+}
+*/
+
+ 
+// 4) Нужно удалить из него чётные числа
+
+/*public class task1 {
+    public static void main (String[] args) {
+        List <Integer> number = new ArrayList <Integer>();
+        number.add(11);
+        number.add(45);
+        number.add(12);
+        number.add(32);
+        number.add(36);
+
+        for (int i = 0; i < number.size(); i++){
+            if (i % 2 == 0) {
+                number.remove(i);
+            }
+        }
+        System.out.println(number);
+    }
+}
+*/
+       
+
